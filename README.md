2b5742757801c2229a27667357727171fc5d4d31
Joseph Rodriguez <joe_rodriguez2@apple.com>
2019-02-20 09:48:35 -0800

    Tiny waitHello Promise function.

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ Then import the basic util
 And try out the function.
 
 `obj.delayedHello(2222)`;
+
+---
+
+`waitHello(...)`
+
+Now you can use the waitHello **Promise** verion like so
+
+```javascript
+obj.waitHello(2222).then(() => /* do stuff here after delay */ console.log('world!'));
--- index.js
+++ index.js
@@ -2,5 +2,15 @@ module.exports = {};
 
 module.exports.delayedHello = (delay = 1200) => {
 	setTimeout(() => console.log('hello'), delay);
-}
+};
+
+module.exports.waitHello = (delay = 1200) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log(`hello (after ${delay})`);
+			resolve();
+		}, delay);
+	});
+};
+
 
